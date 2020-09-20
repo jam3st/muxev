a52fdb8c3338beb92da40119d6caed016684dfa3
Your Name <you@example.com>
2020-09-20 06:42:58 +0200

    Work around "client bug: event processing lagging behind by".

--- src/evdev.cpp
+++ src/evdev.cpp
@@ -1,4 +1,5 @@
 #include <linux/input.h>
+#include <sys/time.h>
 #include <unistd.h>
 #include "evdev.h"
 #include "router.h"
@@ -35,7 +36,12 @@ void EvDev::readAndRoute() {
     }
 }
 
-void EvDev::write(struct input_event const ev[], size_t const count) const {
+void EvDev::write(struct input_event ev[], size_t const count) const {
+    struct timeval now;
+     gettimeofday(&now, NULL);
+    for(auto i = 0 ; i < count; ++ i) {
+	ev[i].time = now;
+    }
     auto const writeSize = count * sizeof(ev[0]);
     auto const nw = ::write(fd, &ev[0], writeSize);
     if(writeSize != nw) {
